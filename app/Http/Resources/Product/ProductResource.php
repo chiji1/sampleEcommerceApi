ebf387cf858f9249a34f2357a336f41918751f9e
chiji1 <okaforchijioke01@gmail.com>
2019-12-01 11:53:48 +0100

    added middleware for product token validation added product request for product POST route

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -20,7 +20,7 @@ class ProductResource extends JsonResource
             'price' => $this->price,
             'stock' => $this->stock,
             'discountPrice' => round($this->price - ($this->price * $this->discount/100)),
-            'rating' => round($this->reviews->sum('rating') / $this->reviews->count(), 1),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('rating') / $this->reviews->count(), 1) : Null,
             'discount' => $this->discount,
 //            'reviews' => $this->reviews,
         ];
